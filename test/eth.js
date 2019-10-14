caa5085123ca7fc2a6456d0f91bb8cf346e679a1
cofepy <cofe@outlook.com>
2019-10-14 18:49:15 +0800

    add ip port set

--- test/eth.js
+++ test/eth.js
@@ -3,6 +3,7 @@ const eth = require('../lib/eth')
 let test = () => {
     //eth.blockNumber().then(console.log)
     //eth.getBalance('0x7dbd9065d38ffbbb27c884baeffca113eda3cb50').then(console.log)
+    eth.set({ ip: "35.224.183.106" })
     eth.getBlockByNumber(0x1).then(console.log, console.log)
 }
 
--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
+//const jsonrpc = require('../lib/jsonrpc')
+//jsonrpc.set({ ip: "35.224.183.106" })
+
 const eth = require('./eth')
 eth.test()
 
-const taraxa = require("./taraxa")
-taraxa.test()
+// const taraxa = require("./taraxa")
+// taraxa.test()
